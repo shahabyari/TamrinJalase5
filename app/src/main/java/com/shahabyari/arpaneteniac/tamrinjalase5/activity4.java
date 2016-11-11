dbaff1b31081ae5dc249b28aa0c2d74f11702d5a
shahabyari <shahab.yari@gmail.com>
2016-11-11 21:37:45 +0330

    Commit - Activity3-comit6

---
+++ app/src/main/java/com/shahabyari/arpaneteniac/tamrinjalase5/activity4.java
@@ -0,0 +1,21 @@
+package com.shahabyari.arpaneteniac.tamrinjalase5;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Administrator on 11/11/2016.
+ */
+
+public class activity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity4);
+
+
+    }
+}
